dc63e2a42ae7cfad1908ce24e256ed187f436112
eliran menachem <eliran.menachem@gmail.com>
2020-01-16 09:32:38 +0200

    question

---
+++ js.js
@@ -0,0 +1,11 @@
+window.onload =  function(){
+    squareInRect()
+}
+
+function squareInRect(){
+
+}// End squareInRect func
+
+var sqrs = squareInRect(5,3); console.log(sqrs); // [3,2,1,1]
+var sqrs = squareInRect(3,5); console.log(sqrs); // [3,2,1,1]
+var sqrs = squareInRect(5,5); console.log(sqrs); // [5]
