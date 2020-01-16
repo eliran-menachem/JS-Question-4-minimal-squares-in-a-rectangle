38f149cca5e8d9208f5ccc058c5b7d576e6fa65b
eliran menachem <eliran.menachem@gmail.com>
2020-01-16 11:45:08 +0200

    solution

--- js.js
+++ js.js
@@ -1,11 +1,46 @@
-window.onload =  function(){
-    squareInRect()
+window.onload = function () {
+    console.log(squareInRect(x, y));
 }
 
-function squareInRect(){
+let x = 3;
+let y = 16;
 
+function squareInRect(x, y) {
+
+    let minStart, maxStart, n, counter, newNum, remainsNum, newMin, endNum;
+    counter = 0;
+    n = 0;
+    minStart = Math.min(x, y);
+    maxStart = Math.max(x, y);
+    console.log('minStart: ' + minStart + ' maxStart: ' + maxStart);
+
+    n = Math.floor(maxStart / minStart);
+    counter += n;
+    newNum = minStart * n;
+    console.log('newNum: ' + newNum);
+    remainsNum = maxStart - newNum;
+    console.log('remainsNum: ' + remainsNum);
+
+    if (remainsNum === 1) {
+        counter += minStart;
+        return 'counter: ' + counter;
+    };
+
+    if (remainsNum === 0) {
+        ; return 'counter: ' + counter;
+    };
+
+    newMin = Math.min(remainsNum, minStart);
+    n = Math.floor(minStart / newMin);
+    counter += n;
+    newNum = newMin * n;
+    endNum = minStart - newMin;
+    console.log('endNum: ' + endNum);
+    counter += newNum
+
+    return 'counter: ' + counter;
 }// End squareInRect func
 
-var sqrs = squareInRect(5,3); console.log(sqrs); // [3,2,1,1]
-var sqrs = squareInRect(3,5); console.log(sqrs); // [3,2,1,1]
-var sqrs = squareInRect(5,5); console.log(sqrs); // [5]
+
+
+
